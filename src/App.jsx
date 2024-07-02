32eb62fadeac871495571d2198893d271dbb777e
DLSiem <pdlsiem17@gmail.com>
2024-07-02 13:57:55 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div className="text-center bg-gray-100 p-4">
+      <h1 className="text-2xl font-bold text-green-700">
+        <span className="text-red-600">Hello</span> World!
+      </h1>
+      <p className="text-sky-900">Let{"'"}s get started!</p>
+    </div>
+  );
+}
+
+export default App;
